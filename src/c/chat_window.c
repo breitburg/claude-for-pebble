625da92a9b7410bd8de1e7a73ab414da23775903
Ilia Breitburg <ilya.breytburg@gmail.com>
2025-10-28 01:01:58 +0100

    Allow continuous scrolling

--- src/c/chat_window.c
+++ src/c/chat_window.c
@@ -403,8 +403,8 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
 }
 
 static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_UP, 100, up_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, 100, down_click_handler);
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
 }
